452720ee2be5b68930123f75808b90fc439a0c6c
mahbubur Rahman <inbx.mahbub@gmail.com>
2024-11-18 16:38:27 +0600

    palindrome : optimized with  ternary operator

--- Top5_JS_DSA_Interview_Questions/palindrome.js
+++ Top5_JS_DSA_Interview_Questions/palindrome.js
@@ -1,4 +1,12 @@
 const isPalindrome = (x) => {
+  if(x < 0 ) return false;
   return x === +x.toString().split("").reverse().join("");
 }
+
+const isPalindrome1 = (x) => {
+  return x < 0 ? false : x === +x.toString().split("").reverse().join("");
+}
+
+const result = isPalindrome(121)
+console.log(result);
 
