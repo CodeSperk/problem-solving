39e5d363e27d00d5b6354510ea0c77d25feedaed
mahbubur Rahman <inbx.mahbub@gmail.com>
2024-11-21 02:39:54 +0600

    Second largest Optimized

--- Array_FAQ_DSA_JS/secondLargestNum.js
+++ Array_FAQ_DSA_JS/secondLargestNum.js
@@ -4,6 +4,23 @@ function secondLargest(arr){
   return sortedArr[1];
 }
 
-const result = secondLargest([10, 5, 10])
+function secondLargestOptimized(arr){
+  let largest = Number.NEGATIVE_INFINITY;
+  let secondLargest = Number.NEGATIVE_INFINITY;
+  // [12, 35, 1, 10, 34, 1]
+  // largest = 35
+  // secondLargest = 34
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] > largest){
+      secondLargest = largest;
+      largest = arr[i];
+    }else if(arr[i] != largest && arr[i] > secondLargest){
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest;
+}
+
+const result = secondLargestOptimized([12, 35, 1, 10, 34, 1])
 console.log(result);
 
